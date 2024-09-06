1684dcf4bf87cc0fceca25f62825cd777112eea4
Mohammad Al-Ahdal <mo@mohammad.dev>
2024-09-06 14:09:34 -0600

    Run Ruff Format

--- app/features/threads.py
+++ app/features/threads.py
@@ -5,19 +5,21 @@ from app.setup import bot
 from app.utils import is_dm, is_mod, server_only_warning
 
 
-@bot.tree.command(name="create-thread", description="Create a thread and move user messages to it.")
+@bot.tree.command(
+    name="create-thread", description="Create a thread and move user messages to it."
+)
 async def create_thread(
-  interaction: discord.Interaction,
-  title: str,
-  starting_message_id: str = None,
-  ending_message_id: str = None,
-  ommitted_messages: str = ""
+    interaction: discord.Interaction,
+    title: str,
+    starting_message_id: str = None,
+    ending_message_id: str = None,
+    ommitted_messages: str = "",
 ) -> None:
     """
     Create a thread and move user messages to it. (Note: this command only searches within the last 100 messages)
-    
+
     This can only be invoked by a mod.
-    
+
     Parameters:
     title: The title of the thread to be created
     starting_message_id: The ID of the message to start the thread from
@@ -32,7 +34,8 @@ async def create_thread(
     # Verify the author is a mod
     if not is_mod(interaction.user):
         await interaction.response.send_message(
-          "You do not have permission to use the create-thread command.", ephemeral=True
+            "You do not have permission to use the create-thread command.",
+            ephemeral=True,
         )
         return
 
@@ -40,23 +43,25 @@ async def create_thread(
     channel = interaction.channel
     thread = await channel.create_thread(name=title)
     await interaction.response.send_message(f"Thread created: {thread.mention}")
-        
+
     # if starting and ending message IDs are provided, move messages between them inclusive to the thread
     if starting_message_id is not None and ending_message_id is not None:
         webhook = await get_webhook(channel)
         starting_message = await channel.fetch_message(int(starting_message_id))
         ending_message = await channel.fetch_message(int(ending_message_id))
         ommitted_ids = ommitted_messages.split(",")
-        message_list = [] # keep track of messages to be checked and potentially sent to the thread
-        added_messages_dict = {} # keep track of messages added to the thread
-        authors_mentioned = [] # keep track of authors mentioned in the thread
-        
+        message_list = []  # keep track of messages to be checked and potentially sent to the thread
+        added_messages_dict = {}  # keep track of messages added to the thread
+        authors_mentioned = []  # keep track of authors mentioned in the thread
+
         # append all messages between starting and ending message inclusive
         message_list.append(starting_message)
-        async for message in channel.history(limit=100, before=ending_message, after=starting_message):
+        async for message in channel.history(
+            limit=100, before=ending_message, after=starting_message
+        ):
             message_list.append(message)
         message_list.append(ending_message)
-        
+
         # send messages to thread
         for message in message_list:
             # ignore messages from bot and messages that are requested to be ommitted
@@ -68,12 +73,24 @@ async def create_thread(
             if message.reference is not None:
                 reference = added_messages_dict.get(message.reference.message_id)
                 if reference is None:
-                    reference = await channel.fetch_message(message.reference.message_id)
+                    reference = await channel.fetch_message(
+                        message.reference.message_id
+                    )
                 content = f"> -# **Replying to**: {reference.jump_url}\n> -# {textwrap.shorten(reference.content, width=30)}\n{message.content}"
-                await webhook.send(content, username=message.author.name, avatar_url=message.author.avatar.url, thread=thread)
+                await webhook.send(
+                    content,
+                    username=message.author.name,
+                    avatar_url=message.author.avatar.url,
+                    thread=thread,
+                )
             # if message is not a reply just send it normally
             else:
-                await webhook.send(message.content, username=message.author.name, avatar_url=message.author.avatar.url, thread=thread)
+                await webhook.send(
+                    message.content,
+                    username=message.author.name,
+                    avatar_url=message.author.avatar.url,
+                    thread=thread,
+                )
             # save the message in case it's a reference to another message in the thread
             sent_message = None
             async for single_message in thread.history(limit=1):
@@ -84,9 +101,10 @@ async def create_thread(
             # in the case we have not "ghosted" the author, send a notifying message to the thread and delete it
             if message.author.id not in authors_mentioned:
                 authors_mentioned.append(message.author.id)
-                notification_message = await thread.send(f"{message.author.mention}") 
+                notification_message = await thread.send(f"{message.author.mention}")
                 await notification_message.delete()
-                
+
+
 async def get_webhook(channel: discord.TextChannel) -> discord.Webhook:
     # check if a webhook is created for the given channel
     webhooks = await channel.webhooks()
