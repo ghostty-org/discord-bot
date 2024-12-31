ff4869237ebf2647463366465d5c99f26122cd1e
trag1c <trag1cdev@yahoo.com>
2024-12-31 01:13:45 +0100

    feat: undo changes if Solved tag gets removed

--- app/components/autoclose.py
+++ app/components/autoclose.py
@@ -1,3 +1,5 @@
+from collections.abc import Iterable
+
 import discord
 
 from app.setup import bot, config
@@ -7,8 +9,15 @@ from app.setup import bot, config
 async def on_thread_update(before: discord.Thread, after: discord.Thread) -> None:
     if before.parent_id != config.HELP_CHANNEL_ID:
         return
-    if not (new_tags := set(after.applied_tags) - set(before.applied_tags)):
+    if (old_tags := set(before.applied_tags)) == (new_tags := set(after.applied_tags)):
         return
-    if not any("solved" in tag.name.casefold() for tag in new_tags):
-        return
-    await after.edit(name=f"[SOLVED] {after.name}", archived=True)
+    if _has_solved_tag(new_tags - old_tags):
+        # A "Solved" tag was added
+        await after.edit(name=f"[SOLVED] {after.name}", archived=True)
+    elif _has_solved_tag(old_tags - new_tags):
+        # A "Solved" tag was removed
+        await after.edit(name=after.name.removeprefix("[SOLVED] "), archived=False)
+
+
+def _has_solved_tag(tags: Iterable[discord.ForumTag]) -> bool:
+    return any("solved" in tag.name.casefold() for tag in tags)
