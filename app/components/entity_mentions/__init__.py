768d5099690a8dd9dcaefb94f4f407a61a3e36ac
trag1c <trag1cdev@yahoo.com>
2025-01-16 00:14:02 +0100

    build: replace `pygithub` with `githubkit`

--- app/components/entity_mentions/__init__.py
+++ app/components/entity_mentions/__init__.py
@@ -1,5 +1,4 @@
-from .cache import REPOSITORIES
 from .fmt import ENTITY_REGEX, load_emojis
 from .integration import reply_with_entities
 
-__all__ = ("ENTITY_REGEX", "REPOSITORIES", "reply_with_entities", "load_emojis")
+__all__ = ("ENTITY_REGEX", "reply_with_entities", "load_emojis")
