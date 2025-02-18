bf89e150ef51165a5fbd3c6cd46ed616e4dd68a8
Kat <65649991+00-kat@users.noreply.github.com>
2025-02-18 17:42:48 +1100

    Add error messages and switch from parsing the URL to `sticker.format`.
    I had previously missed the `format` attribute in stickers; using this
    it is now possible to include an error message instead of silently
    failing.

--- app/utils/webhooks.py
+++ app/utils/webhooks.py
@@ -35,10 +35,13 @@ def _convert_nitro_emojis(s: str, *, force: bool = False) -> str:
     return _EMOJI_REGEX.sub(r, s)
 
 
-async def _get_sticker_embed(sticker: discord.Sticker) -> discord.Embed | None:
+async def _get_sticker_embed(sticker: discord.Sticker) -> discord.Embed:
     # Lottie images can't be used in embeds, unfortunately.
-    if not sticker.url or sticker.url.split("?")[0].endswith(".json"):
-        return None
+    if sticker.format == discord.StickerFormatType.lottie:
+        embed = discord.Embed()
+        embed.set_footer(text="Unable to attach sticker.")
+        embed.color = discord.Color.brand_red()
+        return embed
 
     async with httpx.AsyncClient() as client:
         for u in [
@@ -50,9 +53,16 @@ async def _get_sticker_embed(sticker: discord.Sticker) -> discord.Embed | None:
         ]:
             async with client.stream("GET", u) as r:
                 if r.is_success:
-                    return discord.Embed().set_image(url=u)
+                    embed = discord.Embed().set_image(url=u)
+                    if sticker.format == discord.StickerFormatType.apng:
+                        embed.set_footer(text="Unable to animate sticker.")
+                        embed.color = discord.Color.orange()
+                    return embed
 
-    return None
+    embed = discord.Embed()
+    embed.set_footer(text="Unable to attach sticker.")
+    embed.color = discord.Color.brand_red()
+    return embed
 
 
 def _format_subtext(executor: discord.Member | None, msg_data: MessageData) -> str:
@@ -108,12 +118,7 @@ async def move_message_via_webhook(
         avatar_url=message.author.display_avatar.url,
         allowed_mentions=discord.AllowedMentions.none(),
         files=msg_data.attachments,
-        embeds=message.embeds
-        + [
-            embed
-            for sticker in message.stickers
-            if sticker and (embed := await _get_sticker_embed(sticker)) is not None
-        ],
+        embeds=message.embeds + [await _get_sticker_embed(s) for s in message.stickers],
         thread=thread,
         thread_name=thread_name,
         wait=True,
