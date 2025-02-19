846d0d98a38c20ba6549e0c480f3e7e0cc67e3e7
Kat <65649991+00-kat@users.noreply.github.com>
2025-02-19 11:49:10 +1100

    Satisfy Ruff's benevolent requirements.

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -125,5 +125,5 @@ def escape_special(s: str) -> str:
     escaped = _INVITE_LINK_REGEX.sub(r"<https://\1>", escaped)
     # escape_markdown() doesn't deal with ordered lists.
     return "\n".join(
-        _ORDERED_LIST_REGEX.sub(r"\1\. \2", l) for l in escaped.splitlines()
+        _ORDERED_LIST_REGEX.sub(r"\1\. \2", line) for line in escaped.splitlines()
     )
