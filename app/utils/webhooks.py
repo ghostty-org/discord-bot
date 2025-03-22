8945dbf381f0306ef2300484c1c3acbe8b683ff2
Kat <65649991+00-kat@users.noreply.github.com>
2025-03-22 17:22:03 +1100

    Add a note about elided content in moved replies to forwards.

--- app/utils/webhooks.py
+++ app/utils/webhooks.py
@@ -144,8 +144,10 @@ async def _format_reply(
             description_prefix = "➜ Forwarded\n"
             if ref is discord.utils.MISSING:
                 description = "> *Unable to attach forward.*"
-            else:
+            elif ref.content:
                 description = f"> {ref.content}"
+            else:
+                description = "> *Some forwarded content elided.*"
 
     link = (
         reply.jump_url
