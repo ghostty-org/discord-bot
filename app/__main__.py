021de68ccf91b057850dafa6408aeacc414e03ad
Kat <65649991+00-kat@users.noreply.github.com>
2025-09-10 20:29:33 +1000

    Add logging to files outside components, utils and common.

--- app/__main__.py
+++ app/__main__.py
@@ -1,6 +1,8 @@
 import asyncio
 from contextlib import suppress
 
+from loguru import logger
+
 from app import log
 from app.bot import GhosttyBot
 from app.config import config, gh
@@ -9,7 +11,9 @@ from app.config import config, gh
 async def main() -> None:
     log.setup(config)
 
+    logger.trace("creating a GhosttyBot instance for starting")
     async with GhosttyBot(config, gh) as bot:
+        logger.debug("starting the bot")
         await bot.start(config.token.get_secret_value())
 
 
