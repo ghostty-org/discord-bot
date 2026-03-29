0d057a6d86f4751863f41e81d77697c0dfe49799
trag1c <dev@jakubr.me>
2026-03-30 01:11:39 +0200

    use tmpdir instead of cwd for tests data_dir

--- tests/utils/config.py
+++ tests/utils/config.py
@@ -1,3 +1,4 @@
+from tempfile import gettempdir
 from typing import TYPE_CHECKING, cast
 
 from app.config import Config, config_var
@@ -18,4 +19,4 @@ def config() -> Token[Config]:
     # NOTE: stub out the functions on `bot` as needed (with SimpleNamespace) for
     # execution of the tests.
     bot = cast("GhosttyBot", object())
-    return config_var.set(Config(".env.example", data_dir=".", bot=bot))
+    return config_var.set(Config(".env.example", data_dir=gettempdir(), bot=bot))
